0cb916eb3258357db2e954b0f242a0d359fb2ff5
naman-1998 <namanpanchal17@gmail.com>
2021-12-26 22:45:05 +0530

    Validating POST services at the server side

--- src/main/java/com/example/udemyCourse/springBoot/Entity/User.java
+++ src/main/java/com/example/udemyCourse/springBoot/Entity/User.java
@@ -1,11 +1,17 @@
 
 package com.example.udemyCourse.springBoot.Entity;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 private Integer id;
+
+@Size(min = 3, message = "Name should have atleast 3 characters!!")
 private String name;
+
+@Past
 private Date date;
 
     public User(Integer id, String name, Date date) {
